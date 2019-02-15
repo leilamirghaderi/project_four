a779d0aa2880dedefdf5ab9e9b08539d207b8b64
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-15 16:59:36 -0600

    Generating Migration add_column to Reflection model
    $ rails generate migration AddtitletoReflection title:string
    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_222314) do
+ActiveRecord::Schema.define(version: 2019_02_15_224730) do
 
   create_table "reflections", force: :cascade do |t|
     t.integer "week"
-    t.string "topic"
     t.text "reaction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "interesting_key_words"
     t.integer "student_id"
+    t.string "title"
     t.index ["student_id"], name: "index_reflections_on_student_id"
   end
 
