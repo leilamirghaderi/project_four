eae1c36960dfa39907263d21249c279db16dba1e
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-15 15:23:39 -0600

    Remove interesting_key_words column from Reflection model

--- db/migrate/20190215020616_add_interesting_key_words_to_reflection.rb
+++
@@ -1,6 +0,0 @@
-class AddInterestingKeyWordsToReflection < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reflections, :interesting_key_words, :string
-    add_column :reflections, :string, :string
-  end
-end
