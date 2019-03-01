7243f8c69bce14b79b808c3c873906cd019cb8cd
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-01 16:51:22 -0600

    Remove controller Reflection $ rails destroy controller Reflection

--- app/controllers/reflection_controller.rb
+++
@@ -1,2 +0,0 @@
-class ReflectionController < ApplicationController
-end
--- test/controllers/reflection_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ReflectionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
