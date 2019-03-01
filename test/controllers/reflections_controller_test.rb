9d631e787573a2170c905cadb56edcc03d8a9dfe
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-01 17:01:51 -0600

    Run routes $ rake routes

--- test/controllers/reflections_controller_test.rb
+++ test/controllers/reflections_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ReflectionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @reflection = reflections(:one)
+    @reflection = reflections(:china)
   end
 
   test "should get index" do
