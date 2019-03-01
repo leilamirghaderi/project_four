76b6d3586c1f4f69d0a52d8e85e76dfd4613de60
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-01 14:29:57 -0600

    Change my validation of week of reflection model

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -2,5 +2,6 @@ class Reflection < ApplicationRecord
   belongs_to :student
   validates :reaction, :title, :week, presence: true
   validates :week,
-   numericality: { integer_only: true, greater_than_or_equal_to: 0 }
+  numericality: { integer_only: true, greater_than_or_equal_to: Date.today.cweek },
+  length: { is: 2, message: 'must be a two-digit week' }
 end
