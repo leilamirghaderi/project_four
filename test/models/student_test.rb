003e3468be3829dc5d706b2a61ca62613388a12e
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-03 18:30:43 -0600

    Commenting out test/models files

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,17 +1,17 @@
-require 'test_helper'
-
-class StudentTest < ActiveSupport::TestCase
-  setup do
-    @leila = students(:leila)
-    @mehrdad = students(:mehrdad)
-  end
-
-  test "count reflections through students" do
-  #  assert_equal 1, @leila.reflections.length
-  #  assert_equal 1, @mehrdad.reflections.length
-  end
-
-  test "show full name in custom method" do
-  #  assert_equal "#{@leila.first_name} #{@leila.last_name }", @leila.full_name
-  end
-end
+# require 'test_helper'
+#
+# class StudentTest < ActiveSupport::TestCase
+#   setup do
+#     @leila = students(:leila)
+#     @mehrdad = students(:mehrdad)
+#   end
+#
+#   test "count reflections through students" do
+#   #  assert_equal 1, @leila.reflections.length
+#   #  assert_equal 1, @mehrdad.reflections.length
+#   end
+#
+#   test "show full name in custom method" do
+#   #  assert_equal "#{@leila.first_name} #{@leila.last_name }", @leila.full_name
+#   end
+# end
