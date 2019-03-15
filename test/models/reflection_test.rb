9a572cdacd7c47f1c9e3ef3e1fcd9983a06882f0
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-14 22:32:46 -0500

    Add test for week in reflection_test.rb file

--- test/models/reflection_test.rb
+++ test/models/reflection_test.rb
@@ -21,31 +21,18 @@ class ReflectionTest < ActiveSupport::TestCase
   @reflection.save
   assert_equal @reflection.student_name, students(:leila).name
   end
-  # setup do
-  #   @one = reflections(:one)
-  # end
-  #
-  # test "reflection is not valid without a unique title" do
-  #   @two = reflections(:two)
-  #   @one.title = @two.title
-  #   assert @one.invalid?
-  # end
-  #
-  # test "week must be positive" do
-  #   @one.week = -1
-  #   assert @one.invalid?
-  #
-  #   @one.week = 0
-  #   assert @one.invalid?
-  #
-  #    @one.week = 1
-  #   assert @one.valid?
-  # end
-  # test "reflection title must be unique on a given week and reaction" do
-  #   @one = reflections(:one)
-  #   # Duplicate the app reflection's title and identifier from HCI
-  #   @one.reaction = reflections(:two).reaction
-  #   @one.title = reflections(:two).title
-  #   assert_not @one.valid?
-  # end
+  setup do
+    @one = reflections(:one)
+  end
+
+  test "week must be positive" do
+    @one.week = -1
+    assert @one.invalid?
+
+    @one.week = 0
+    assert @one.invalid?
+
+     @one.week = 1
+    assert @one.valid?
+  end
 end
