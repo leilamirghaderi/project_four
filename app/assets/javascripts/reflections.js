5002305a022d129a8374eb87c4935a01a7ce7a24
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-25 17:52:44 -0500

    create haml.rb file

---
+++ app/assets/javascripts/reflections.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded',function(){
+//   var title = document.querySelector('#reflection_title');
+//   var title = document.querySelector('#reflection_reaction');
+//
+//   function set_reaction() {
+//     reaction.value = title.value.replace(/\w/g,'-').toLowerCase();
+//   }
+//
+//   title.addEventListener('keyup',set_reaction);
+//
+//   reaction.addEventListener('keyup')
+//
+// })
