1b34ec3848237cf608ab254c450a67c008088430
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-31 21:36:57 -0500

    Add student_id in validates in reflection.rb

--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -1,6 +1,6 @@
 class Reflection < ApplicationRecord
   belongs_to :student
-  validates :reaction, :title, :week, presence: true
+  validates :reaction, :title, :week, :student_id, presence: true
   validates :week,
   numericality: { integer_only: true, greater_than_or_equal_to: 1 }
 
