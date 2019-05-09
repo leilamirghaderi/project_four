33fbd03293eb73347b22a35195c4ec72504ff492
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-05-08 20:41:05 -0500

    Migrate from github authentication to google authentication $ rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  get '/logout', to: 'sessions#logout', as: 'logout'
+  get '/login', to: 'sessions#login', as: 'login'
+
+  get '/auth/google_oauth2', as: 'google_oauth2_auth'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   resources :reflections, constraints: { id: /\d+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'reflections#index'
